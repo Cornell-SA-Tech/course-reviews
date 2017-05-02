fa5f632bebe198567b213995f72dfcfcbcfaefc4
Daniela Gottesman <odedgottesman@Odeds-MacBook-Pro.local>
2017-05-01 23:24:07 -0400

    added fonts to template, edited sliders, rounded containers

--- imports/api/classes.js
+++ imports/api/classes.js
@@ -167,7 +167,7 @@ if (Meteor.isServer) {
     }
 
   //COMMENT THESE OUT AFTER THE FIRST METEOR BUILD!!
-  //Classes.remove({});
-  //Subjects.remove({});
-  //initDB(true);
+  // Classes.remove({});
+  // Subjects.remove({});
+  // initDB(true);
 }
